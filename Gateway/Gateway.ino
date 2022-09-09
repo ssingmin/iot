dde58041d8d727552d7a5c6eb6ffe9a806d778ec
ssingmin <ssingmin@gmail.com>
2022-09-09 16:56:21 +0900

    before write with cha

--- Gateway/Gateway.ino
+++ Gateway/Gateway.ino
@@ -4,8 +4,12 @@
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h>
 
-#define untilID 1//1~n
+#define untilID 2//1~n
 #define Cycle 10000//0~n
+
+char SMstr[9][1024];
+
+String test = "";
 
 unsigned char tmp_humi;
 float tmp_temp[3];
@@ -18,24 +22,32 @@ typedef struct _Smodule {
   float Temp[3];
 } Smodule;
 
+Smodule SM[9] = {
+{"",1, 0,{0.0, 0.0, 0.0}},
+{"",2, 0,{0.0, 0.0, 0.0}}, 
+{"",3, 0,{0.0, 0.0, 0.0}}, 
+{"",4, 0,{0.0, 0.0, 0.0}}, 
+{"",5, 0,{0.0, 0.0, 0.0}}, 
+{"",6, 0,{0.0, 0.0, 0.0}}, 
+{"",7, 0,{0.0, 0.0, 0.0}}, 
+{"",8, 0,{0.0, 0.0, 0.0}}, 
+{"",9, 0,{0.0, 0.0, 0.0}}, 
+};
 SimpleTimer timer1;
 SimpleTimer timer2;
 
 WiFiClient wifiClient;  //추가
 
 SoftwareSerial HC12(D7, D6); // HC-12 TX Pin은 D2(RX용)에, HC-12 RX Pin을 D3(TX용)
+
+int updatedone_flag = 0;
 int Jsontostring_flag = 0;
 int receiveData_flag = 0;
+//int request_flag = 257;
 
-
-
-int request_flag = 0;
-
-int sendID = 0;
+int sendID = 1;
 int checkID = 0;
 
-
-//unsigned char checksum;
 unsigned char parsing_ID;
 int post_counter = 0;
 int request_counter = 0;
@@ -51,34 +63,44 @@ int toggle = 0; //for indicator led
 
 void updateDB(void) {//interval 3s
 
-//  if (WiFi.status() == WL_CONNECTED) {    // WiFi가 연결되어 있을 경우 HTTP 접속
-//    HTTPClient http;  // HTTPClient 클래스의 오브섹트 선언
-//    String server = "http://15.165.203.24:8992/insert_sensor_data";
-//
-//    http.begin(wifiClient, server);//추가
-//    http.addHeader("Content-Type", "application/json");
-//    
-//    int httpCode = http.POST(stringout);// 여기가 진짜
-//
-//    if (httpCode > 0) // 정상적으로 요청이 된 경우
-//    {
-//      String payload = http.getString();   // 응답을 수신
-//      Serial.println(payload);            // 수신된 응답 내용을 시리얼 통신을 통해 출력
-//    }
-//
-//    http.end(); // HTTP 접속 종료
-//  }
+ if (WiFi.status() == WL_CONNECTED) {    // WiFi가 연결되어 있을 경우 HTTP 접속
+   HTTPClient http;  // HTTPClient 클래스의 오브섹트 선언
+   String server = "http://15.165.203.24:8992/insert_sensor_data";
+
+   http.begin(wifiClient, server);//추가
+   http.addHeader("Content-Type", "application/json");
+   
+   int httpCode = http.POST(SMstr[0]);// 여기가 진짜
+
+   if (httpCode > 0) // 정상적으로 요청이 된 경우
+   {
+     String payload = http.getString();   // 응답을 수신
+     Serial.println(payload);            // 수신된 응답 내용을 시리얼 통신을 통해 출력
+   }
+   updatedone_flag = 1;
+   http.end(); // HTTP 접속 종료
+ }
 }
 
 void request_data()//interval 1s untilID까지 보냈으면 더이상 보내지 말기 
 {
-  if (request_flag == 1) {
-    sendID--; // if gateway don't receive message, it will repeat request
+  if (checkID>=untilID && updatedone_flag==1) {
+    updatedone_flag = 0;
+    checkID = 0;
+    sendID = 1;
   }
-  request_flag = 1;
-  request_counter = 0;
-  checkID = sendID;
-  requestID(sendID++);
+       Serial.println(String(checkID)+" checkID");      // 읽어서 HC-12 모듈로 전달합니다
+      Serial.println(String(sendID)+" sendID");      // 읽어서 HC-12 모듈로 전달합니다
+  if (checkID+1 != sendID) {
+    sendID--; // if gateway don't receive message, it will repeat request
+    Serial.println("tqtq sendID");
+  }
+
+  if(sendID <= untilID){
+    request_counter = 0;
+    //checkID = sendID;
+    requestID(sendID++);
+    }
 }
 
 void setup () {
@@ -86,111 +108,61 @@ void setup () {
   HC12.begin(9600);               // HC-12 시리얼 통신 속도 설정
   Serial.begin(115200);        // PC에서 모니터링하기 위한 시리얼 통신 시작
   WiFi.begin(ssid, password); // WiFi 접속
-
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting..");
   }
-  timer1.setInterval(3000, updateDB); // 주기적으로 DB업데이트
-  timer2.setInterval(1000, request_data); // 주기적으로 DB업데이트
+  timer1.setInterval(10000, updateDB); // 주기적으로 DB업데이트
+  timer2.setInterval(500, request_data); // 주기적으로 DB업데이트
+ 
 }
 
-  String test = "";
-
-  for(int i=1;i<=untilID;i++){
-    Smodule SM[i] = {"",i, 0,{0.0, 0.0, 0.0}}; //untilID 까지 자동 선언 
-  }
 void loop() {
 
-  //timer1.run(); //updateDB
+  timer1.run(); //updateDB
   timer2.run(); //request_data
   receiveData_flag = receiveData();
 
   if(receiveData_flag>0){
+    checkID = receiveData_flag;
+    Serial.println(checkID);
     SM[receiveData_flag].Smid = receiveData_flag;
     SM[receiveData_flag].Humi = tmp_humi;
     for(int i=0;i<3;i++){SM[receiveData_flag].Temp[i] = tmp_temp[i];}
+    
     //if(receiveData_flag){update_flag = 1;}
+
+       if (Jsontostring(SM[receiveData_flag])) {
+     //Jsontostring_flag++;
+     test += "ID";
+     test += String(SM[receiveData_flag].Smid);
+     test += ": top(";
+     test += SM[receiveData_flag].Temp[0];
+     test += ") mid(";
+     test += SM[receiveData_flag].Temp[1];
+     test += ") bot("; 
+     test += SM[receiveData_flag].Temp[2];
+     test += ") humi(";
+     test += SM[receiveData_flag].Humi;
+     test += ")";
+
+     Serial.println(test);      // 읽어서 HC-12 모듈로 전달합니다
+     //Serial.println(SM[receiveData_flag].stringout);      // 읽어서 HC-12 모듈로 전달합니다
+   }
   }
   for(int i=0;i<3;i++){tmp_temp[i] = 0;} //init
   tmp_humi = 0;//init
-
-    if (Jsontostring(SM)) {
-      Jsontostring_flag++;
-      test += "ID";
-      test += String(checkID);
-      test += ": top(";
-      test += tmp_temp[0];
-      test += ") mid(";
-      test += tmp_temp[1];
-      test += ") bot(";
-      test += tmp_temp[2];
-      test += ") humi(";
-      test += tmp_humi;
-      test += ")";
-
-      Serial.println(test);      // 읽어서 HC-12 모듈로 전달합니다
-    }
 
   if (post_counter++ > Cycle) {
     post_counter = 0;
     toggle ^= 1;
     digitalWrite(14, toggle);
+    delay(10);
   }
 }
 
 ///////////////////////////////////loop end////////////////////////////////
-int receiveData()
-{
-  ////parsing////
-  if (HC12.available() > 0)
-  {
-    parsing_buf[parsing_counter++] = HC12.read();
-
-    if (parsing_counter >= 11)
-    {
-      parsing_counter = 0;
-      request_flag = parsingMSG(parsing_buf);
-      for (int i = 0; i < 11; i++) {
-        parsing_buf[i] = 0; //clear buf
-      }
-    }
-    return request_flag;
-  }
-  return 0;
-  ///////////////
-}
-
-int parsingMSG(unsigned char buf[])
-{
-  unsigned char checksum;
-
-  if ((buf[0] == 0xff) && (buf[1] == 0xfe))
-  {
-    checksum = buf[2];
-    for (int i = 4; i < 11; i++) {
-      checksum += buf[i];
-    }
-    if (buf[3] == (checksum ^ 0xff))
-    {
-      for (int i = 0; i < 3; i++) {
-        tmp_temp[i] = ((float)(buf[(i * 2) + 4] * 256 + buf[(i * 2) + 5]) / 100);
-      }
-      tmp_humi = buf[10];
-      ///////////////////////////////////
-      if (checkID > untilID) {
-        checkID = 0;
-      }
-      if (sendID > untilID) {
-        sendID = 0;
-      }
-      return buf[2];//return id
-    }
-    return 0;
-  }
-  return 0;
-}
 
 
 void requestID(unsigned char i)
@@ -211,6 +183,57 @@ void requestID(unsigned char i)
   Serial.println(" ");
 }
 
+
+int receiveData()
+{
+  ////parsing////
+int request_flag;
+
+  if (HC12.available() > 0)
+  {
+    parsing_buf[parsing_counter++] = HC12.read();
+    if (parsing_counter >= 11)
+    {
+      parsing_counter = 0;
+      request_flag = parsingMSG(parsing_buf);
+      for (int i = 0; i < 11; i++) {
+        parsing_buf[i] = 0; //clear buf
+      }
+    }
+    return request_flag;
+  }
+  return 0;
+  
+  ///////////////
+}
+
+int parsingMSG(unsigned char buf[])
+{
+  unsigned char checksum;
+
+  if ((buf[0] == 0xff) && (buf[1] == 0xfe))
+  {
+    checksum = buf[2];
+    for (int i = 4; i < 11; i++) {
+      checksum += buf[i];
+    }
+    if (buf[3] == (checksum ^ 0xff))
+    {
+      for (int i = 0; i < 3; i++) {
+        tmp_temp[i] = ((float)(buf[(i * 2) + 4] * 256 + buf[(i * 2) + 5]) / 100);
+      }
+      tmp_humi = buf[10];
+      ///////////////////////////////////
+      return buf[2];//return id
+    }
+
+    return 0;
+  }
+  return 0;
+}
+
+
+
 //void Jsontostring(char *s1, float *Temp, unsigned char Humi)
 int Jsontostring(Smodule module)
 {
@@ -220,14 +243,37 @@ int Jsontostring(Smodule module)
   doc["frequency"] = 10;
   JsonArray ports = doc["data_list"].to<JsonArray>();
   JsonObject root = ports.createNestedObject();
-  root["sensor_id"] = module.Smid;
+  //root["sensor_id"] = module.Smid;
+  switch(module.Smid){
+   case 1 :
+    root["sensor_id"] = "sensor_a";break;
+  case 2 :
+    root["sensor_id"] = "sensor_b";break;
+  case 3 :
+    root["sensor_id"] = "sensor_c";break;
+  case 4 :
+    root["sensor_id"] = "sensor_d";break;
+  case 5 :
+    root["sensor_id"] = "sensor_e";break;
+  case 6 :
+    root["sensor_id"] = "sensor_f";break;
+  case 7 :
+    root["sensor_id"] = "sensor_g";break;
+  case 8 :
+    root["sensor_id"] = "sensor_h";break;
+  case 9 :
+    root["sensor_id"] = "sensor_i";break;
+ }
+  
+  root["sensor_id"] = "sensor_b";
   root["top"] = module.Temp[0];
   root["middle"] = module.Temp[1];
   root["bottom"] = module.Temp[2];
   root["humidity"] = module.Humi;
-
-  serializeJson(doc, module.stringout, 1024);
-  //  serializeJson(doc, Serial);
+  serializeJson(doc, SMstr[module.Smid], 1024);
+  //serializeJson(doc, module.stringout, 1024);
+  //serializeJson(doc, Serial);
+  //Serial.println("Tlqkffhak");
   Serial.println("");
   return 1;
 }
--- SModule/SModule.ino
+++ SModule/SModule.ino
@@ -1,162 +1,194 @@
 #include <OneWire.h>
 #include <SoftwareSerial.h>
-#define buf_size 11
-#define parsing_size 5
+#define BUFFER_SIZE 11
+#define PARSING_SIZE 5
+#define SMODULE_ID 2
+#define LED_PORT 13
 
-#define SModule_ID 0
-
-#define blink_led 13
-
-SoftwareSerial HC12(2,3); // HC-12 TX Pin, HC-12 RX Pin
-int resetflag = 0;//임시
+SoftwareSerial HC12(2, 3); // HC-12 TX Pin, HC-12 RX Pin
+int resetflag = 0;         //임시
 
 int polling_flag = 0;
 
 ///////////////////////////////humi/////////////////////////////////////////
-const int AirValue = 600;   //you need to replace this value with Value_1
-const int WaterValue = 350;  //you need to replace this value with Value_2
+const int AirValue = 600;   // you need to replace this value with Value_1
+const int WaterValue = 350; // you need to replace this value with Value_2
 
 int soilMoistureValue = 0;
-int soilmoisturepercent=0;
+int soilmoisturepercent = 0;
 
 //////////////////////////////////////////////////////////////////////////////
 //데이터 배열을 일률적으로 해야할듯 그래서 다른데에서 오는 데이터도 받고 버릴 수 있게 지금 버퍼가 차이가 나면서 꼬이는 문제가 발생하네 ㅅㅄㅄㅄㅂ
 
 /////////////////////////////////////////protocol/////////////////////////////////////////
-//head(0):0xff, head(1):0xfe, id(2):0x00~0a, checksum(3):0xff, checksum=!(id(2)+datasum(4~10))
-//A0temp_H(4):0xff, A0temp_L(5):0xff, A1temp_H(6):0xff, A1temp_L(7):0xff, A2temp_H(8):0xff, A2temp_L(9):0xff, humi(10):0xff, 
+// head(0):0xff, head(1):0xfe, id(2):0x00~0a, checksum(3):0xff, checksum=!(id(2)+datasum(4~10))
+// A0temp_H(4):0xff, A0temp_L(5):0xff, A1temp_H(6):0xff, A1temp_L(7):0xff, A2temp_H(8):0xff, A2temp_L(9):0xff, humi(10):0xff,
 unsigned char parsing_ID;
 int parsing_counter = 0;
 unsigned char checksum = 0;
 unsigned char rev_checksum = 0;
-unsigned char RF_buf[buf_size] = {0xff,0xfe,SModule_ID,checksum,4,5,6,7,8,9,0};
-unsigned char parsing_buf[parsing_size] = {0,};
+unsigned char RF_buf[BUFFER_SIZE] = {0xff, 0xfe, SMODULE_ID, checksum, 4, 5, 6, 7, 8, 9, 0};
+unsigned char parsing_buf[PARSING_SIZE] = {
+    0,
+};
 //////////////////////////////////////////////////////////////////////////////////////////
 
+// int DS18S20_Pin = 2;                             //온도센서를 2번 핀으로 연결
+int DS18S20_topPin = 14; // a0=14
+int DS18S20_midPin = 15; // a1=15
+int DS18S20_botPin = 16; // a2=16
 
-//int DS18S20_Pin = 2;                             //온도센서를 2번 핀으로 연결
-int DS18S20_topPin = 14;//a0=14
-int DS18S20_midPin = 15;//a1=15
-int DS18S20_botPin = 16;//a2=16
+int toggle = 0; // for indicator led
 
-int toggle=0;//for indicator led
+float temperature[3] = {
+    0,
+};
 
-float temperature[3] = {0,};
+int tmp = 0;
 
-int tmp=0;
+OneWire ds[3] = {14, 15, 16}; // 14=a0, 15=a1, 16=a2
 
- OneWire ds[3] = {14, 15, 16};                         //14=a0, 15=a1, 16=a2
-
-
-void setup(){
-  HC12.begin(9600); // Serial port to HC12
-  pinMode(13, OUTPUT);
-  Serial.begin(115200);
-} 
-
-
-void loop(){
-  
-  for(int i=0;i<3;i++)
-  {
-    temperature[i] = getTemp(ds[i]);                 //온도 측정 후 변수에 저장
-    RF_buf[(i*2)+4] = (((int)(temperature[i]*100))>>8);
-    RF_buf[(i*2)+5] = ((int)(temperature[i]*100));
-    //tmp = (int)(temperature[i]*100);
-    //Serial.println(tmp);
-  }
-    
-  soilMoistureValue = analogRead(A3);  //put Sensor insert into soil
-  soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-  RF_buf[10]=soilmoisturepercent;
-  
-  ///checksum///
-  checksum=SModule_ID;  
-  for(int i=4;i<11;i++){checksum+=RF_buf[i];}
-  RF_buf[3] = (~checksum);
-  //////////////
-
-
-////paring////
-
-  if (HC12.available() > 0) 
-  {
-    parsing_buf[parsing_counter++] = HC12.read();
-
-    if(parsing_counter>=parsing_size)
-    {
-      for(int i=0;i<5;i++){Serial.print(String(parsing_buf[i])+" ");}
-      Serial.println(" ");
-      parsing_counter = 0;
-      
-      if((parsing_buf[0]==0xfe) && (parsing_buf[1]==0xfe))
-      {
-        parsing_ID = parsing_buf[2];
-        rev_checksum = parsing_buf[2]+parsing_buf[4];
-
-        if((parsing_ID == SModule_ID) && (parsing_buf[3] == (rev_checksum^0xff)))
-        {
-          for(int i=0;i<11;i++){Serial.print(String(RF_buf[i])+" ");}
-          Serial.println(" ");
-          for(int i=0;i<buf_size;i++){HC12.write(RF_buf[i]);}
-          
-          for(int i=3;i<=buf_size;i++){RF_buf[i] = 0;}   //clear RF_buf
-          for(int i=0;i<=parsing_size;i++){parsing_buf[i] = 0;}   //clear parsing_buf
-          
-        }
-      }
-    }
-  }
-/////////////
-  
-  polling_flag++;
-  if(polling_flag > 10)
-  {
-    Serial.println("waiting for your command...");
-    polling_flag = 0;
-    toggle^=1;
-    digitalWrite(blink_led, toggle);   // 상태확인
-    if((resetflag++)>10){asm volatile (" jmp 0");}//임시방법
-  }
+void setup()
+{
+    HC12.begin(9600); // Serial port to HC12
+    pinMode(13, OUTPUT);
+    Serial.begin(115200);
 }
 
+void loop()
+{
 
+    for (int i = 0; i < 3; i++)
+    {
+        temperature[i] = getTemp(ds[i]); //온도 측정 후 변수에 저장
+        RF_buf[(i * 2) + 4] = (((int)(temperature[i] * 100)) >> 8);
+        RF_buf[(i * 2) + 5] = ((int)(temperature[i] * 100));
+        // tmp = (int)(temperature[i]*100);
+        // Serial.println(tmp);
+    }
+
+    soilMoistureValue = analogRead(A3); // put Sensor insert into soil
+    soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+    RF_buf[10] = soilmoisturepercent;
+
+    /// checksum//
+    checksum = SMODULE_ID;
+    for (int i = 4; i < 11; i++)
+    {
+        checksum += RF_buf[i];
+    }
+    RF_buf[3] = (~checksum);
+    //////////////
+
+    ////paring////
+
+    if (HC12.available() > 0)
+    {
+        parsing_buf[parsing_counter++] = HC12.read();
+
+        if (parsing_counter >= PARSING_SIZE)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Serial.print(String(parsing_buf[i]) + " ");
+            }
+            Serial.println(" ");
+            parsing_counter = 0;
+
+            if ((parsing_buf[0] == 0xfe) && (parsing_buf[1] == 0xfe))
+            {
+                parsing_ID = parsing_buf[2];
+                rev_checksum = parsing_buf[2] + parsing_buf[4];
+                if ((parsing_ID == SMODULE_ID) && (parsing_buf[3] == (rev_checksum ^ 0xff)))
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        Serial.print(String(RF_buf[i]) + " ");
+                    }
+                    Serial.println(" ");
+                    for (int i = 0; i < BUFFER_SIZE; i++)
+                    {
+                        HC12.write(RF_buf[i]);
+                    }
+
+                    for (int i = 3; i <= BUFFER_SIZE; i++)
+                    {
+                        RF_buf[i] = 0;
+                    } // clear RF_buf
+                    for (int i = 0; i <= PARSING_SIZE; i++)
+                    {
+                        parsing_buf[i] = 0;
+                    } // clear parsing_buf
+                }
+                else {
+                    parsing_counter = 0;
+                    for (int i=0; i<BUFFER_SIZE; i++) {
+                        parsing_buf[i] = 0;
+                    }
+                    HC12.flush();
+                }
+            }
+        }
+    }
+    /////////////
+        if ((resetflag++) > 100)
+        {
+            asm volatile(" jmp 0");
+        } //임시방법
+    #if 0
+    polling_flag++;
+    if (polling_flag > 10)
+    {
+        Serial.println("waiting for your command...");
+        polling_flag = 0;
+        // toggle^=1;
+        digitalWrite(LED_PORT, toggle); // 상태확인
+        if ((resetflag++) > 10)
+        {
+            asm volatile(" jmp 0");
+        } //임시방법
+    }
+    #endif
+}
 
 ////////////////////////////////////////////////////////////////
 
+float getTemp(OneWire ds)
+{ //온도 측정 후 반환하는 함수
+    byte data[12];
+    byte addr[8];
+    if (!ds.search(addr))
+    {
+        ds.reset_search();
+        return -1000;
+    }
+    if (OneWire::crc8(addr, 7) != addr[7])
+    {
+        Serial.println("CRC is not valid!");
+        return -1000;
+    }
+    if (addr[0] != 0x10 && addr[0] != 0x28)
+    {
+        Serial.print("Device is not recognized");
+        return -1000;
+    }
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);
+    byte present = ds.reset();
+    ds.select(addr);
+    ds.write(0xBE);
 
+    for (int i = 0; i < 9; i++)
+    {
+        data[i] = ds.read();
+    }
 
-float getTemp(OneWire ds){      //온도 측정 후 반환하는 함수
- byte data[12];
- byte addr[8];
- if ( !ds.search(addr)) {
-   ds.reset_search();
-   return -1000;
- }
- if ( OneWire::crc8( addr, 7) != addr[7]) {
-   Serial.println("CRC is not valid!");
-   return -1000;
- }
- if ( addr[0] != 0x10 && addr[0] != 0x28) {
-   Serial.print("Device is not recognized");
-   return -1000;
- }
- ds.reset();
- ds.select(addr);
- ds.write(0x44,1);                                   
- byte present = ds.reset();
- ds.select(addr);  
- ds.write(0xBE); 
- 
- for (int i = 0; i < 9; i++) { 
-  data[i] = ds.read();                                                          
- }
- 
- ds.reset_search(); 
- byte MSB = data[1];
- byte LSB = data[0];
- float tempRead = ((MSB << 8) | LSB); 
- float TemperatureSum = tempRead / 16; 
- 
- return TemperatureSum;                                                                    
+    ds.reset_search();
+    byte MSB = data[1];
+    byte LSB = data[0];
+    float tempRead = ((MSB << 8) | LSB);
+    float TemperatureSum = tempRead / 16;
+
+    return TemperatureSum;
 }
