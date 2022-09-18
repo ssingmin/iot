be1a0d650056bbfab698e44fba9b56061b1e6653
ssingmin <ssingmin@gmail.com>
2022-09-18 15:26:22 +0900

    add Scan id function

--- SModule/SModule.ino
+++ SModule/SModule.ino
@@ -2,10 +2,11 @@
 #include <SoftwareSerial.h>
 #define BUFFER_SIZE 11
 #define PARSING_SIZE 5
-#define SMODULE_ID 3
+//#define SMODULE_ID 1
 #define LED_PORT 13
 
 SoftwareSerial HC12(2, 3); // HC-12 TX Pin, HC-12 RX Pin
+int Scanid = 0;
 int resetflag = 0;         //임시
 
 int polling_flag = 0;
@@ -18,7 +19,6 @@ int soilMoistureValue = 0;
 int soilmoisturepercent = 0;
 
 //////////////////////////////////////////////////////////////////////////////
-//데이터 배열을 일률적으로 해야할듯 그래서 다른데에서 오는 데이터도 받고 버릴 수 있게 지금 버퍼가 차이가 나면서 꼬이는 문제가 발생하네 ㅅㅄㅄㅄㅂ
 
 /////////////////////////////////////////protocol/////////////////////////////////////////
 // head(0):0xff, head(1):0xfe, id(2):0x00~0a, checksum(3):0xff, checksum=!(id(2)+datasum(4~10))
@@ -27,8 +27,8 @@ unsigned char parsing_ID;
 int parsing_counter = 0;
 unsigned char checksum = 0;
 unsigned char rev_checksum = 0;
-unsigned char RF_buf[BUFFER_SIZE] = {0xff, 0xfe, SMODULE_ID, checksum, 4, 5, 6, 7, 8, 9, 0};
-unsigned char TMP_buf[10] = {0,};
+unsigned char RF_buf[BUFFER_SIZE] = {0xff, 0xfe, 0, checksum, 4, 5, 6, 7, 8, 9, 0};
+unsigned char TMP_buf[10] = {0,};//Transmit buf
 unsigned char parsing_buf[PARSING_SIZE] = {
     0,
 };
@@ -41,9 +41,7 @@ int DS18S20_botPin = 16; // a2=16
 
 int toggle = 0; // for indicator led
 
-float temperature[3] = {
-    0,
-};
+float temperature[3] = {0,};
 
 int tmp = 0;
 
@@ -52,40 +50,27 @@ OneWire ds[3] = {14, 15, 16}; // 14=a0, 15=a1, 16=a2
 void setup()
 {
     HC12.begin(9600); // Serial port to HC12
-    pinMode(13, OUTPUT);
     Serial.begin(115200);
+    
+    pinMode(13, OUTPUT);
+    for(int i=7;i<=12;i++){pinMode(i, INPUT_PULLUP);}//for scan id
+    
+    for(int i=7;i<=12;i++){
+        if(digitalRead(i)==0){
+          Scanid = (i-6);   
+          RF_buf[2] = Scanid;
+          break;
+          }
+        }
 }
 
 void loop()
 {
-
-    for (int i = 0; i < 3; i++)
-    {
-        temperature[i] = getTemp(ds[i]); //온도 측정 후 변수에 저장
-        RF_buf[(i * 2) + 4] = (((int)(temperature[i] * 100)) >> 8);
-        RF_buf[(i * 2) + 5] = ((int)(temperature[i] * 100));
-        // tmp = (int)(temperature[i]*100);
-        // Serial.println(tmp);
-    }
-
-    soilMoistureValue = analogRead(A3); // put Sensor insert into soil
-    soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-    RF_buf[10] = soilmoisturepercent;
-
-    /// checksum//
-    checksum = SMODULE_ID;
-    for (int i = 4; i < 11; i++)
-    {
-        checksum += RF_buf[i];
-    }
-    RF_buf[3] = (~checksum);
-    //////////////
-
+    delay(1);
     ////paring////
 
     if (HC12.available() > 0)
     {
-        
         TMP_buf[parsing_counter++] = HC12.read();
 
         if (parsing_counter >= PARSING_SIZE)
@@ -109,8 +94,6 @@ void loop()
                 }
             }
             
-            
-            
             for (int i = 0; i < 5; i++)
             {
                 Serial.print(String(parsing_buf[i]) + " ");
@@ -122,26 +105,17 @@ void loop()
             {
                 parsing_ID = parsing_buf[2];
                 rev_checksum = parsing_buf[2] + parsing_buf[4];
-                if ((parsing_ID == SMODULE_ID) && (parsing_buf[3] == (rev_checksum ^ 0xff)))
-                {
-                    for (int i = 0; i < 11; i++)
-                    {
-                        Serial.print(String(RF_buf[i]) + " ");
-                    }
-                    Serial.println(" ");
-                    for (int i = 0; i < BUFFER_SIZE; i++)
-                    {
-                        HC12.write(RF_buf[i]);
-                    }
 
-                    for (int i = 3; i <= BUFFER_SIZE; i++)
-                    {
-                        RF_buf[i] = 0;
-                    } // clear RF_buf
-                    for (int i = 0; i <= PARSING_SIZE; i++)
-                    {
-                        parsing_buf[i] = 0;
-                    } // clear parsing_buf
+                if ((parsing_ID == Scanid) && (parsing_buf[3] == (rev_checksum ^ 0xff)))//check checksum 
+                {
+                    ScanData();
+                    for (int i = 0; i < 11; i++) {Serial.print(String(RF_buf[i]) + " ");}
+                    Serial.println(" ");
+                    for (int i = 0; i < BUFFER_SIZE; i++) {HC12.write(RF_buf[i]);}
+
+                    for (int i = 3; i <= BUFFER_SIZE; i++){RF_buf[i] = 0;} // clear RF_buf
+                    for (int i = 0; i <= PARSING_SIZE; i++){parsing_buf[i] = 0;} // clear parsing_buf
+
                 }
                 else {
                     parsing_counter = 0;
@@ -154,28 +128,42 @@ void loop()
         }
     }
     /////////////
-        if ((resetflag++) > 10)
-        {
-            Serial.println("reset!!!");
-            delay(100);
-            asm volatile(" jmp 0");
-        } //임시방법
-    #if 0
-    polling_flag++;
-    if (polling_flag > 10)
-    {
-        Serial.println("waiting for your command...");
-        polling_flag = 0;
-        // toggle^=1;
-        digitalWrite(LED_PORT, toggle); // 상태확인
-        if ((resetflag++) > 10)
-        {
-            asm volatile(" jmp 0");
-        } //임시방법
-    }
-    #endif
+       if ((resetflag++) > 5000)
+       {
+           Serial.println("reset!!!");
+           digitalWrite(LED_PORT, HIGH); // 상태확인
+           delay(30);
+           digitalWrite(LED_PORT, LOW); // 상태확인
+           asm volatile(" jmp 0");
+       }
 }
 
+////////////////////////////////////////////////////////////////
+
+void ScanData()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        temperature[i] = getTemp(ds[i]); //온도 측정 후 변수에 저장
+        RF_buf[(i * 2) + 4] = (((int)(temperature[i] * 100)) >> 8);
+        RF_buf[(i * 2) + 5] = ((int)(temperature[i] * 100));
+        // tmp = (int)(temperature[i]*100);
+        // Serial.println(tmp);
+    }
+
+    soilMoistureValue = analogRead(A3); // put Sensor insert into soil
+    soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+    RF_buf[10] = soilmoisturepercent;
+
+        /// transmit checksum///
+     checksum = Scanid;
+     for (int i = 4; i < 11; i++)
+     {
+         checksum += RF_buf[i];
+     }
+     RF_buf[3] = (~checksum);
+    //////////////
+}
 ////////////////////////////////////////////////////////////////
 
 float getTemp(OneWire ds)
